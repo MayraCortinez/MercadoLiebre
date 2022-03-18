b8ace3c858ddce96040c827ec2a68c311ea5543f
MayraCortinez <mayrautopia@gmail.com>
2022-03-18 15:50:54 -0300

    Terminado

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const path = require('path');
 const app = express();
 const port = 3030;
 
-
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.get('/',(req,res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html' )))
 
